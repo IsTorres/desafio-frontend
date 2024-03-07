1568d7eec83959cfec589e3d1669da8dd8c14339
istorres <ismaeltorres612@gmail.com>
2024-03-07 20:12:04 -0300

    feat: add menu component

---
+++ src/components/Menu.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default function Menu() {
+  return (
+    <div>
+      Menu{" "}
+      <ul>
+        <li>
+          <a href="/pages/product/">Products</a>
+        </li>
+        <li>
+          <a href="/pages/shopping-cart/">ShoppingCart</a>
+        </li>
+      </ul>
+    </div>
+  );
+}

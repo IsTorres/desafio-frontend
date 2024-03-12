6867a8cb3f34c63f8f83bcf13d64acf6a7b779f2
istorres <ismaeltorres612@gmail.com>
2024-03-11 22:43:39 -0300

    feat; update menu component

--- src/components/GlobalComponents/Menu.tsx
+++ src/components/GlobalComponents/Menu.tsx
@@ -1,17 +1,20 @@
 import MarvelComicsIcon from "../../../public/MarvelComicsIcon.png";
+import MarvelLogo from "../../../public/MarvelLogo.png";
+
+import { NavMenu } from "./styles";
 export default function Menu() {
   return (
-    <div>
-      <nav style={{ display: "flex" }}>
-        <div>
+    <NavMenu>
+      <nav className="navigation">
+        <div className="nav-itens">
           <a href="/">
-            <img src={MarvelComicsIcon} />
+            <img src={MarvelComicsIcon} alt="logo marvel comics" />
           </a>
         </div>
-        <div>
+        <div className="nav-itens">
           <a href="/pages/shopping-cart/">ShoppingCart</a>
         </div>
       </nav>
-    </div>
+    </NavMenu>
   );
 }

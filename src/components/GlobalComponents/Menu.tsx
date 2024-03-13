b7ddaa12bc57d66784f508e91c95b879c9140c5d
istorres <ismaeltorres612@gmail.com>
2024-03-13 10:29:21 -0300

    feat: add logo to menu

--- src/components/GlobalComponents/Menu.tsx
+++ src/components/GlobalComponents/Menu.tsx
@@ -1,18 +1,21 @@
 import MarvelComicsIcon from "../../../public/MarvelComicsIcon.png";
-import MarvelLogo from "../../../public/MarvelLogo.png";
+import { FaShoppingCart } from "react-icons/fa";
 
 import { NavMenu } from "./styles";
 export default function Menu() {
   return (
     <NavMenu>
       <nav className="navigation">
-        <div className="nav-itens">
+        <div className="nav-itens logo  ">
           <a href="/">
             <img src={MarvelComicsIcon} alt="logo marvel comics" />
           </a>
         </div>
-        <div className="nav-itens">
-          <a href="/shopping-cart/">ShoppingCart</a>
+        <div className="nav-itens cart">
+          <span className="buy-count-dot">0</span>
+          <a href="/shopping-cart/">
+            <FaShoppingCart width={"3em"} height={"3em "} />
+          </a>
         </div>
       </nav>
     </NavMenu>

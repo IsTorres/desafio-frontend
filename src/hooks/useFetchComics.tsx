460b51250716e9ab3b66422ac8a6cfcf56ee1b75
istorres <ismaeltorres612@gmail.com>
2024-03-13 18:47:03 -0300

    feat: create hooks

---
+++ src/hooks/useFetchComics.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react";
+import { fetchComicsWithParams } from "../service";
+import { ComicsPeriodes } from "../types/Comic";
+
+const useFetchComics = () => {
+  const [loading, setLoading] = useState(false);
+  const [comics, setComics] = useState<ComicsPeriodes>({
+    thisWeek: [],
+    nextWeek: [],
+    lastWeek: [],
+    thisMonth: [],
+  });
+
+  const fetchComics = async () => {
+    const newDateDescriptor: (keyof ComicsPeriodes)[] = [
+      "thisWeek",
+      "nextWeek",
+      "lastWeek",
+      "thisMonth",
+    ];
+    setLoading(true);
+    try {
+      const comicsData = await Promise.all(
+        newDateDescriptor.map((descriptor) => fetchComicsWithParams(descriptor))
+      );
+      const comicsObject: ComicsPeriodes = newDateDescriptor.reduce(
+        (acc, descriptor, index) => {
+          acc[descriptor] = comicsData[index];
+          return acc;
+        },
+        {} as ComicsPeriodes
+      );
+      setComics(comicsObject);
+    } catch (error) {
+      console.error("Error loading comics:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return { loading, comics, fetchComics };
+};
+
+export default useFetchComics;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,39 +1,10 @@
-import { useEffect, useState } from "react";
-import { fetchComicsWithParams } from "../../service";
-import { ComicsPeriodes } from "../../types/Comic";
+import { useEffect } from "react";
 import ComicsSections from "../../components/HomePage/ComicsSections";
 import { ComicList } from "../../components/HomePage/styles";
+import useFetchComics from "../../hooks/useFetchComics";
 
 export default function Home() {
-  const [loading, setLoading] = useState<boolean>(true);
-  const [comics, setComics] = useState<ComicsPeriodes>();
-
-  const fetchComics = async () => {
-    const newDateDescriptor: (keyof ComicsPeriodes)[] = [
-      "thisWeek",
-      "nextWeek",
-      "lastWeek",
-      "thisMonth",
-    ];
-    setLoading(true);
-    try {
-      const comicsData = await Promise.all(
-        newDateDescriptor.map((descriptor) => fetchComicsWithParams(descriptor))
-      );
-      const comicsObject: ComicsPeriodes = newDateDescriptor.reduce(
-        (acc, descriptor, index) => {
-          acc[descriptor] = comicsData[index];
-          return acc;
-        },
-        {} as ComicsPeriodes
-      );
-      setComics(comicsObject);
-    } catch (error) {
-      console.error("Error loading comics:", error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const { loading, comics, fetchComics } = useFetchComics();
 
   useEffect(() => {
     fetchComics();

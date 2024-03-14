c4b200e61799dce9fdfa475da9297a02ab6e0abc
istorres <ismaeltorres612@gmail.com>
2024-03-14 00:25:32 -0300

    feat: simplify buy process

--- src/components/GlobalComponents/BuyButton.tsx
+++ src/components/GlobalComponents/BuyButton.tsx
@@ -1,16 +1,20 @@
 import { FaPlusCircle, FaMinusCircle } from "react-icons/fa";
 import { Buttons, BuyButton, ActionButtons } from "./styles";
+import { GlobalContext } from "../../contexts/GlobalContext";
+import { useContext } from "react";
 
-export default function BuyButtonComponent() {
+export default function BuyButtonComponent({ id }: { id: number }) {
+  const { addProduct } = useContext(GlobalContext);
+
   return (
     <Buttons>
-      <ActionButtons>
+      {/* <ActionButtons onClick={() => removeProduct(id)}>
         <FaMinusCircle />
-      </ActionButtons>
-      <BuyButton>Buy</BuyButton>
-      <ActionButtons>
+      </ActionButtons> */}
+      <BuyButton onClick={() => addProduct(id)}>Add to Cart</BuyButton>
+      {/* <ActionButtons onClick={() => addProduct(id)}>
         <FaPlusCircle />
-      </ActionButtons>
+      </ActionButtons> */}
     </Buttons>
   );
 }

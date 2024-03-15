c7fece7931a58e1e391b5d91a3162587d44e64b9
istorres <ismaeltorres612@gmail.com>
2024-03-15 13:22:07 -0300

    trying to fix 404 pages error

--- vite.config.ts
+++ vite.config.ts
@@ -8,14 +8,17 @@ const outDir = resolve(__dirname, "dist");
 // https://vitejs.dev/config/
 export default defineConfig({
   root,
-  build: {
-    outDir,
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(root, "index.html"),
-      },
-    },
-  },
+  // build: {
+  //   // outDir,
+  //   emptyOutDir: true,
+  //   rollupOptions: {
+  //     input: {
+  //       main: resolve(root, "index.html"),
+  //       product: resolve(__dirname, 'product/index.html'),
+  //       shoppingCart: resolve(__dirname, 'shoppingCart/index.html'),
+  //       checkout: resolve(__dirname, 'checkout/index.html'),
+  //     },
+  //   },
+  // },
   plugins: [react()],
 });

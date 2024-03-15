2b1919e4a37dfaaed7755e40fc7ee86b552eadc7
istorres <ismaeltorres612@gmail.com>
2024-03-15 13:03:07 -0300

    add SpeedInsights

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import App from "./App.tsx";
 import "./index.css";
 import GlobalProvider from "./contexts/GlobalContext.tsx";
 import CreditCardProvider from "./contexts/CreditCardContext.tsx";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GlobalProvider>
       <CreditCardProvider>
         <App />
+        <SpeedInsights />
       </CreditCardProvider>
     </GlobalProvider>
   </React.StrictMode>

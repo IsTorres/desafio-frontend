dba158155bac52dae60a2e0bc94f50a769333933
istorres <ismaeltorres612@gmail.com>
2024-03-15 13:54:02 -0300

    remove speed insights

--- src/main.tsx
+++ src/main.tsx
@@ -4,14 +4,12 @@ import App from "./App.tsx";
 import "./index.css";
 import GlobalProvider from "./contexts/GlobalContext.tsx";
 import CreditCardProvider from "./contexts/CreditCardContext.tsx";
-import { SpeedInsights } from "@vercel/speed-insights/next";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <GlobalProvider>
       <CreditCardProvider>
         <App />
-        <SpeedInsights />
       </CreditCardProvider>
     </GlobalProvider>
   </React.StrictMode>

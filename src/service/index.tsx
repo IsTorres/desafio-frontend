8fee5996fa45f90fec132d9bf1988a39d60b2b37
istorres <ismaeltorres612@gmail.com>
2024-03-15 15:30:21 -0300

    set .env

--- src/service/index.tsx
+++ src/service/index.tsx
@@ -2,23 +2,27 @@ import axios, { AxiosInstance } from "axios";
 import md5 from "md5";
 import { Comic } from "../types/Comic";
 
-const VITE_PUBLIC_API_URL = "https://gateway.marvel.com";
-const VITE_PUBLIC_API_KEY = "4a32cc9eabad270186710953f8d81c44";
-const VITE_PRIVATE_API_KEY = "56b7aeb535744220be8e1b374bcb4fec429a5353";
+// const VITE_PUBLIC_API_URL = "https://gateway.marvel.com";
+// const VITE_PUBLIC_API_KEY = "4a32cc9eabad270186710953f8d81c44";
+// const VITE_PRIVATE_API_KEY = "56b7aeb535744220be8e1b374bcb4fec429a5353";
 
 const ts = Number(new Date());
 
-const hash = md5(ts + VITE_PRIVATE_API_KEY + VITE_PUBLIC_API_KEY);
+const hash = md5(
+  ts +
+    import.meta.env.VITE_PRIVATE_API_KEY +
+    import.meta.env.VITE_PUBLIC_API_KEY
+);
 
 const api: AxiosInstance = axios.create({
-  baseURL: `${VITE_PUBLIC_API_URL}/v1/public/`,
+  baseURL: `${import.meta.env.VITE_PUBLIC_API_URL}/v1/public/`,
 });
 
 const defaultParams = {
   format: "comic",
   formatType: "comic",
   ts,
-  apikey: VITE_PUBLIC_API_KEY,
+  apikey: import.meta.env.VITE_PUBLIC_API_KEY,
   hash,
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,30 @@
 import { resolve } from "path";
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 const root = resolve(__dirname, "src");
 const outDir = resolve(__dirname, "dist");
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  root,
-  build: {
-    outDir,
-    emptyOutDir: true,
-    //   rollupOptions: {
-    //     input: {
-    //       main: resolve(root, "index.html"),
-    //       product: resolve(__dirname, 'product/index.html'),
-    //       shoppingCart: resolve(__dirname, 'shoppingCart/index.html'),
-    //       checkout: resolve(__dirname, 'checkout/index.html'),
-    //     },
-    // },
-  },
-  plugins: [react()],
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    // vite config
+    root,
+    build: {
+      outDir,
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: resolve(root, "index.html"),
+        },
+      },
+    },
+    plugins: [react()],
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+  };
 });
